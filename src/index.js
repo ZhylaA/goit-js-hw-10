66f56d6604317d694259c2e23d4bb38eee694a49
ZhylaA <zak2010@i.ua>
2022-02-23 00:08:01 +0200

    hw10

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+// const countryListItem = document.querySelector('.country-list_item');
+countryList.style.listStyle = "none";
+// countryListItem.style.display = "flex";
 
-// countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-countryInput.addEventListener('input', onSearch);
+countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+// countryInput.addEventListener('input', onSearch);
 
 function onSearch(e) {
     e.preventDefault();
-    // const searchQuery = e.currentTarget.elements.query.value;
-    const searchQuery = e.currentTarget.value.trim();
+    const searchQuery = e.target.value.trim();
     console.log(searchQuery);
     if (searchQuery === "") { countryList.innerHTML = "";}
     else {
@@ -33,26 +35,26 @@ function onSearch(e) {
         }
         else {
             clearInput();
-            // countryList.innerHTML = "";
             countryList.innerHTML = countryListBig(e);}
     }        
 };
     
 // console.log(fetchCountries)
 function countryListSmall(countries) {
-    const markupCountryListSmall = countries.map((country) => {return `<li></li><img src="${country.flags.svg}" alt="flag" width="80">
-    <p>${country.name}</p></li>`;
+    const markupCountryListSmall = countries.map((country) => {return `<li class="country-list_item"><img src="${country.flags.svg}" alt="flag" width="80">
+    <p>${country.name.official}</p></li>`;
     }).join("");
     return markupCountryListSmall;
 };
 function countryListBig(countries) {
-    const markupCountryListBig = countries.map((country) => {return `<li></li><img src="${country.flags.svg}" alt="flag" width="80">
-    <p>${country.name}</p>
-    <p>${country.capital}</p>
-    <p>${country.population}</p>
-    <p>${country.lang}</p>
+    const markupCountryListBig = countries.map((country) => {return `<li><img src="${country.flags.svg}" alt="flag" width="80">
+    <p>COUNTRY: ${country.name.official}</p>
+    <p>CAPITAL: ${country.capital}</p>
+    <p>POPULATION: ${country.population}</p>
+    <p>LANGUAGES: ${Object.values(country.languages)}</p>
     </li>`;
     }).join("");
+    console.log(Object.values)
     return markupCountryListBig;
 };
 function clearInput() {
